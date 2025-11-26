782a6e65c2db71bf2920ab0729b8c9e2c06a5ceb
Pawlos Habtemariam <pawloshabtez@gmail.com>
2025-11-26 12:57:43 +0300

    Time: 0 ms (100.00%) | Memory: 12.4 MB (79.81%) - LeetSync

--- 231-power-of-two/power-of-two.py
+++ 231-power-of-two/power-of-two.py
@@ -6,7 +6,7 @@ class Solution(object):
         """
         if n <= 0:
             return False
-        while n > 1:
+        while n > 1: #if it is 0 the loop doesnt stop so it should be 1 to stop at 1 and output True
             if n % 2 != 0:
                 return False
             n = n / 2
