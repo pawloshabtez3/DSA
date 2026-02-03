ea3bf28d527ba153511f2a8fcc0e9eeb0ed18006
Pawlos Habtemariam <pawloshabtez@gmail.com>
2026-02-03 22:12:26 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (33.82%) - LeetSync

---
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def inorderTraversal(self, root):
+        res = []
+        
+        def dfs(node):
+            if not node:
+                return
+            dfs(node.left)
+            res.append(node.val)
+            dfs(node.right)
+        
+        dfs(root)
+        return res
